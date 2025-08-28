4866ace25d15cdf2627d346754f972dabda4cddd
scp103 <kokor.yevhen@gmail.com>
2025-08-29 00:25:27 +0300

    Update 123dgsdgasgsedh4.lua

--- 123dgsdgasgsedh4.lua
+++ 123dgsdgasgsedh4.lua
@@ -1,5 +1,5 @@
 -- Об'єднане мод-меню (AIM + ESP + Noclip + BunnyHop + Fly + FOV + Sky) | Для KRNL
--- Додай Touch підтримку:
+aimFOVSliderFrame.InputBegan:Connect(function(input)
 local Players = game:GetService("Players")
 local RunService = game:GetService("RunService")
 local UserInputService = game:GetService("UserInputService")
